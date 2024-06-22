0023b2c03b0ef12643b6ae07ec61d6dd0565d234
Rudy-I28 <rivan.jamjachi@gmail.com>
2024-06-22 00:10:40 -0500

    Curso 1 - Desafio 3: Alura Latam
    Repositorios de los trabajos de ONE

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+//Ejercicio 1
+let contador = 1;
+
+while(contador <= 10){
+    alert(contador);
+    contador = contador +1;
+}
+*/
+/*
+//Ejercicio 2
+let contador = 10;
+while(contador >= 0){
+    alert(contador);
+    contador = contador -1;
+}
+*/
+
+//Ejercicio 3
+let numero = prompt('Ingrese un numero del 1 al 10: ');
+let contador = 0;
+
+while(numero >= contador){
+    alert(contador);
+    contador = contador + 1;
+}
+
